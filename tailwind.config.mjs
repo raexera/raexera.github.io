4b0379e6f4d197043d78109fe3040ae5d65b352f
rxyhn <rxyhn.dev@gmail.com>
2024-09-25 20:10:31 +0700

    chore: update tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,16 @@ export default {
   darkMode: ["class"],
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    container: {
+      center: true,
+      padding: "15px",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
+    },
     extend: {
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
