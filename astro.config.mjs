43b5d46266186dbb37b8afbbac23cb8b60a80c58
raexera <raexera@gmail.com>
2026-05-09 03:25:21 +0700

    feat: updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://raexera.is-a.dev",
-  integrations: [react(), tailwind(), icon()],
+  integrations: [react(), icon(), tailwindcss()],
 });
